d3f70a19f80a1dba6b665da9ce62e26fe64c4207
Vickyviki027 <102538568+Vickyviki027@users.noreply.github.com>
2022-11-16 23:16:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const initializeDbAndServer = async () => {
               filename: dbPath,
               driver: sqlite3.Database
       });
-       app.listen(3000, () => {
-       console.log("Server Running at http://localhost:5000/");
+       app.listen(4000, () => {
+       console.log("Server Running at http://localhost:4000/");
       });
    }
     catch (error) {
